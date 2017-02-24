58ed071479b3f222421cb6a22e94ebb5bea9c1ca
Ryan Collins <rccproductions@mac.com>
2017-02-24 15:54:41 -0500

    Fix: remove wrapping animation component in block hero

--- src/ContentBlocks/BlockHero/BlockHero.js
+++ src/ContentBlocks/BlockHero/BlockHero.js
@@ -1,7 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import Box from 'grommet/components/Box';
 import Markdown from 'grommet/components/Markdown';
-import Animate from 'grommet/components/Animate';
 import Headline from 'grommet/components/Headline';
 import Button from 'grommet/components/Button';
 
@@ -12,14 +11,16 @@ export default class BlockHero extends Component {
     this.calculateRandomIndex = this.calculateRandomIndex.bind(this);
     this.state = {
       currentIndex: 0,
-      visible: false,
     };
   }
   componentWillMount() {
     this.setRandomIndex();
-    setTimeout(() => {
-      this.setState({ visible: true });
-    }, 1000);
+  }
+  // Recursively calculate next random nextIndex
+  calculateRandomIndex() {
+    const { carousel } = this.props;
+    const randomIndex = Math.floor(Math.random() * carousel.length);
+    return randomIndex;
   }
   setRandomIndex() {
     const nextIndex = this.calculateRandomIndex();
@@ -27,55 +28,43 @@ export default class BlockHero extends Component {
       currentIndex: nextIndex,
     });
   }
-  calculateRandomIndex() {
-    const { carousel } = this.props;
-    const randomIndex = Math.floor(Math.random() * carousel.length);
-    return randomIndex;
-  }
   render() {
     const { carousel, imageSize, headline, content } = this.props;
     const size = imageSize ? imageSize.toLowerCase() : 'large';
-    const { currentIndex, visible } = this.state;
+    const { currentIndex } = this.state;
     const image = carousel[currentIndex].image;
     const imagePath = image && image.path
       ? image.path
       : '';
 
     return (
-      <Animate
-        keep
-        visible={visible}
-        enter={{ animation: 'fade', duration: 1000, delay: 0 }}
-        leave={{ animation: 'fade', duration: 1000, delay: 0 }}
+      <Box
+        align="center"
+        justify="center"
+        size={{ height: size, width: size }}
+        full={size === 'full'}
+        style={{ backgroundSize: 'cover', backgroundPosition: 'center', width: '100vw' }}
+        texture={imagePath}
       >
         <Box
+          pad="medium"
           align="center"
           justify="center"
-          size={{ height: size, width: size }}
-          full={size === 'full'}
-          style={{ backgroundSize: 'cover', backgroundPosition: 'center', width: '100vw' }}
-          texture={imagePath}
+          colorIndex="grey-4-a"
         >
-          <Box
-            pad="medium"
-            align="center"
-            justify="center"
-            colorIndex="grey-4-a"
-          >
-            <Headline align="center" size="large" strong>
-              {headline || ''}
-            </Headline>
-            <Markdown
-              content={content || ''}
-              components={{
-                p: { props: { size: 'large', margin: 'small', align: 'center' } },
-                h2: { props: { strong: true, align: 'center' } },
-              }}
-            />
-            <Button label="Get Started" path="/brand-central/main" />
-          </Box>
+          <Headline align="center" size="large" strong>
+            {headline || ''}
+          </Headline>
+          <Markdown
+            content={content || ''}
+            components={{
+              p: { props: { size: 'large', margin: 'small', align: 'center' } },
+              h2: { props: { strong: true, align: 'center' } },
+            }}
+          />
+          <Button label="Get Started" path="/brand-central/main" />
         </Box>
-      </Animate>
+      </Box>
     );
   }
 }
