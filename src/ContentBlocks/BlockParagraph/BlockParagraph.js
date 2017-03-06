d0814cafdc8c00c23bb6a75c566ca81533d2eb3d
Ryan Collins <rccproductions@mac.com>
2017-03-06 11:45:04 -0500

    Feat: version bump with new content blocks

--- src/ContentBlocks/BlockParagraph/BlockParagraph.js
+++ src/ContentBlocks/BlockParagraph/BlockParagraph.js
@@ -6,7 +6,11 @@ export default function BlockParagraph({ content }) {
     <Markdown
       content={content} components={{
         p: { props: { size: 'medium', margin: 'none' } },
-        h2: { props: { strong: true } },
+        h1: { props: { margin: 'none' } },
+        h2: { props: { margin: 'none' } },
+        h3: { props: { margin: 'none' } },
+        h4: { props: { margin: 'none' } },
+        h5: { props: { margin: 'none' } },
       }}
     />
   );
