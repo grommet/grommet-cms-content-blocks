3b8477b9435d68827b9644652517641ca7949837
Ryan Collins <rccproductions@mac.com>
2017-03-13 16:39:29 -0400

    Feat: update block carousel to work with size full

--- src/ContentBlocks/BlockCarousel/BlockCarousel.js
+++ src/ContentBlocks/BlockCarousel/BlockCarousel.js
@@ -4,15 +4,21 @@ import Carousel from 'grommet/components/Carousel';
 
 export default function BlockCarousel({ carousel, imageSize }) {
   const size = imageSize ? imageSize.toLowerCase() : 'large';
-  const slides = carousel.map((slide, index) =>
-    <Box
-      key={`slide-${index}`}
-      size={{ height: size, width: size }}
-      full={size === 'full' ? 'horizontal' : ''}
-      style={{ backgroundPosition: '50% 50%', backgroundSize: 'contain', width: '100vw', maxWidth: '100%' }}
-      texture={slide.image.path}
-    />,
-  );
+  const slides = carousel.map((slide, index) => {
+    const sizeObject = size === 'full' ? 'full' : {
+      height: size,
+      width: size,
+    };
+    return (
+      <Box
+        key={`slide-${index}`}
+        size={sizeObject}
+        full={size === 'full' ? true : ''}
+        style={{ backgroundPosition: '50% 50%', backgroundSize: 'contain', width: '100vw' }}
+        texture={slide.image.path}
+      />
+    );
+  });
 
   return (
     <Box style={{ maxWidth: '100vw', width: '100%' }}>
