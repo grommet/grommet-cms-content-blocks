5aa37924ec2692c05bc6892e67d7245bd851d61f
Ryan Collins <rccproductions@mac.com>
2017-03-28 16:06:38 -0400

    Feat: update the url validation to allow mailto: in block button

--- src/ContentBlocks/BlockButton/validation.js
+++ src/ContentBlocks/BlockButton/validation.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const urlRe = new RegExp("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
+const urlRe = new RegExp("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.|(mailto:)){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
 
 const validLength = str => str && str.length > 0;
 const validUrl = url => url && urlRe.test(url);
