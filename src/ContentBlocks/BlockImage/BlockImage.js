78f4570dbb21ae75444d618f7bd0ef96778c3afc
Ryan Collins <rccproductions@mac.com>
2017-04-17 11:25:45 -0400

    Feat: add blockparagraph to blockimage so that paragraph is medium

--- src/ContentBlocks/BlockImage/BlockImage.js
+++ src/ContentBlocks/BlockImage/BlockImage.js
@@ -1,13 +1,15 @@
 import React, { PropTypes } from 'react';
-import Markdown from 'grommet/components/Markdown';
 import unescape from 'unescape';
+import BlockParagraph from '../BlockParagraph';
 import colorMap from './colorMap';
 import Div from './div';
 import Image from './image';
 
 export default function BlockImage({ content, alt, image, borderColor }) {
   const unescapedContent = unescape(content || '');
-  const caption = (unescapedContent && unescapedContent !== '') ? <Markdown content={content} /> : '';
+  const caption = (unescapedContent && unescapedContent !== '')
+    ? <BlockParagraph content={content} />
+    : '';
   const path = image && image.path ? image.path : '';
   const color = (borderColor && borderColor !== 'none') ? colorMap[borderColor] : '';
   if (path === '') {
