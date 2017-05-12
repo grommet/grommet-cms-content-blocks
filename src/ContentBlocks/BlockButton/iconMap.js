54ffcd89a46e3581780d939956a4261d7806fd03
Ryan Collins <rccproductions@mac.com>
2017-05-12 16:37:38 -0400

    Feat: refactor map to be more modular for block button iconPicker

---
+++ src/ContentBlocks/BlockButton/iconMap.js
@@ -0,0 +1,23 @@
+// @flow
+import React from 'react';
+import MailIcon from 'grommet/components/icons/base/Mail';
+import LinkNextIcon from 'grommet/components/icons/base/LinkNext';
+import ChatIcon from 'grommet/components/icons/base/Chat';
+import ShareIcon from 'grommet/components/icons/base/Share';
+import DocumentWordIcon from 'grommet/components/icons/base/DocumentWord';
+import AttachmentIcon from 'grommet/components/icons/base/Attachment';
+import DocumentZipIcon from 'grommet/components/icons/base/DocumentZip';
+import DocumentDownloadIcon from 'grommet/components/icons/base/DocumentDownload';
+import PrintIcon from 'grommet/components/icons/base/Print';
+
+export default {
+  primary: <LinkNextIcon />,
+  email: <MailIcon />,
+  chat: <ChatIcon />,
+  share: <ShareIcon />,
+  attachment: <AttachmentIcon />,
+  word: <DocumentWordIcon />,
+  zip: <DocumentZipIcon />,
+  download: <DocumentDownloadIcon />,
+  print: <PrintIcon />,
+};
--- src/ContentBlocks/BlockButton/iconPicker.js
+++ src/ContentBlocks/BlockButton/iconPicker.js
@@ -1,33 +1,11 @@
 // @flow
-
-import React from 'react';
-import MailIcon from 'grommet/components/icons/base/Mail';
-import LinkNextIcon from 'grommet/components/icons/base/LinkNext';
-import ChatIcon from 'grommet/components/icons/base/Chat';
-import ShareIcon from 'grommet/components/icons/base/Share';
-import DocumentWordIcon from 'grommet/components/icons/base/DocumentWord';
-import AttachmentIcon from 'grommet/components/icons/base/Attachment';
-import DocumentZipIcon from 'grommet/components/icons/base/DocumentZip';
-import DocumentDownloadIcon from 'grommet/components/icons/base/DocumentDownload';
-import PrintIcon from 'grommet/components/icons/base/Print';
+import iconMap from './iconMap';
 import type { IconType } from './BlockButtonForm';
 
 type Props = {
   icon: IconType,
 }
 
-const map = {
-  primary: <LinkNextIcon />,
-  email: <MailIcon />,
-  chat: <ChatIcon />,
-  share: <ShareIcon />,
-  attachment: <AttachmentIcon />,
-  word: <DocumentWordIcon />,
-  zip: <DocumentZipIcon />,
-  download: <DocumentDownloadIcon />,
-  print: <PrintIcon />,
-};
-
-export default function ({ icon }: Props) {
-  return map[icon];
+export default function ({ icon }: Props): React$Element<*> {
+  return iconMap[icon];
 }
