bf005e6fd93293cb26a994a5e51c5c6135f62a97
karatehcops <alex.mejias@hpe.com>
2017-10-18 13:58:27 -0400

    Remove content layout padding adjustments.

--- src/ContentBlocks/ContentLayoutEngine/styles.js
+++ src/ContentBlocks/ContentLayoutEngine/styles.js
@@ -24,10 +24,6 @@ const responsiveBoxStyles = (props) => {
 
 // eslint-disable-next-line no-unused-vars
 export const Box = styled(({ hideForResponsive, ...rest }) => <GrommetBox {...rest} />)`
-  padding: 0;
-  margin: 0;
-  padding-right: 24px;
-  padding-bottom: 48px;
   ${props => responsiveBoxStyles(props)}
 `;
 
@@ -35,44 +31,5 @@ export const Section = styled(GrommetSection)`
   & > div:empty {
     padding: 0;
     margin: 0;
-  }
-
-  &:first-child > div:first-child {
-    margin-top: 0;
-    padding-top: 0;
-  }
-
-  ul {
-    margin-bottom: 24px;
-    padding-bottom: 0;
-
-    li:last-child {
-      p:last-child {
-        margin-bottom: 0;
-      }
-    }
-  }
-
-  &:last-child > div:last-child,
-  div > div, {
-    margin-bottom: 0;
-    padding-bottom: 0;
-    
-    p:last-child,
-    div:last-child:not(.grommetux-tile),
-    h2:last-child,
-    h3:last-child,
-    h4:last-child,
-    h5:last-child,
-    ul:last-child {
-      margin-bottom: 0;
-      padding-bottom: 0;
-
-      li:last-child {
-        p:last-child {
-          margin-bottom: 0;
-        }
-      }
-    }
   }
 `;
